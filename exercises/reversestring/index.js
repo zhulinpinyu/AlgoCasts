bf1ec75a2b5202ed1c1c437c7ac0a764816bcf3c
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-24 11:36:44 +0800

    reversestring 方法二 （包含两种实现）

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// Solution #1
+// const reverse = (str) => {
+//   return str.split('').reverse().join('')
+// }
+
+// Solution #2-1
+// const reverse = (str) => {
+//   let reversed = ''
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i]
+//   }
+//   return reversed
+// }
+
+//Solution #2-2
 const reverse = (str) => {
-  return str.split('').reverse().join('')
+  let reversed = ''
+  for(let character of str){
+    reversed = `${character}${reversed}`
+  }
+  return reversed
 }
 
 module.exports = reverse;
