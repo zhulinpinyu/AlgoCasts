0254dec696c660cd41d5bc3438762160fe4d7beb
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-24 16:57:01 +0800

    高频字符检测

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,10 +1,30 @@
-// --- Directions
+// 高频字符检测 --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+  let max = 0
+  let maxCharacter = ''
+
+  for (let c of str) {
+    if(charMap[c]){
+      charMap[c]++
+    }else{
+      charMap[c] = 1
+    }
+  }
+
+  for(let k in charMap){
+    if(charMap[k] > max){
+      max = charMap[k]
+      maxCharacter = k
+    }
+  }
+  return maxCharacter
+}
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,14 @@
 const maxChar = require('./index');
 
-test('maxChar function exists', () => {
+test('maxChar函数是否定义', () => {
   expect(typeof maxChar).toEqual('function');
 });
 
-test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
+test('找出使用频率最高的字符', () => {
+  //expect(maxChar('a')).toEqual('a');
   expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
 });
 
-test('Works with numbers in the string', () => {
+test('找出使用频率最高的数字字符', () => {
   expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
 });
