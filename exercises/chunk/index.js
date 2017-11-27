4255b6bcc42ae55d610335be72c8c2208ca66dee
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-27 09:45:47 +0800

    数组再分组 方法二
    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,18 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+//Solution #1
+// function chunk(array, size) {
+//   const ret = []
+//   const ret_length = Math.ceil(array.length/size)
+//   for(let ni=0; ni < ret_length; ni++){
+//     let temp = []
+//     for(let i=0; i<size; i++){
+//       const v = array.shift()
+//       if(v) temp.push(v)
+//     }
+//     ret.push(temp)
+//   }
+//   return ret
+// }
+
+//Solution #2
 function chunk(array, size) {
-  const ret = []
-  const ret_length = Math.ceil(array.length/size)
-  for(let ni=0; ni < ret_length; ni++){
-    let temp = []
-    for(let i=0; i<size; i++){
-      const v = array.shift()
-      if(v) temp.push(v)
+  const chunked = []
+  for (ele of array) {
+    /*
+    * 此一种算法实现核心为: 数组是引用类型，
+    * 所以修正last, chunked 也会随之修改
+    */
+    const last = chunked[chunked.length - 1]
+    if(!last||last.length === size){
+      chunked.push([ele])
+    }else{
+      last.push(ele)
     }
-    ret.push(temp)
   }
-  return ret
+  return chunked
 }
 
 module.exports = chunk;
