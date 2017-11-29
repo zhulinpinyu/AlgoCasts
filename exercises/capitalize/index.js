3d5c123b1d4ab61ee76bd676c17adfc16db26859
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-29 17:04:42 +0800

    英文句子中所有单词 首字母大写 方法二

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,8 +7,23 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// Solution #1
+// function capitalize(str) {
+//   return str.split(" ").map(e => e[0].toUpperCase()+e.slice(1)).join(" ")
+// }
+
+// Solution #2
 function capitalize(str) {
-  return str.split(" ").map(e => e[0].toUpperCase()+e.slice(1)).join(" ")
+  let ret = str[0].toUpperCase()
+
+  for(let i=1; i<str.length; i++){
+    if(str[i-1] === " "){
+      ret += str[i].toUpperCase()
+    }else{
+      ret += str[i]
+    }
+  }
+  return ret
 }
 
 module.exports = capitalize;
