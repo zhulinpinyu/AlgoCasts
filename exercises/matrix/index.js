cd49d592352566cbdb81375e46d34d79fc7a1fec
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-14 10:23:19 +0800

    n*n 回旋矩阵输出
    妙

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+//  matrix(5)
+// [[1,   2,  3,  4, 5],
+//  [16, 17, 18, 19, 6],
+//  [15, 24, 25, 20, 7],
+//  [14, 23, 22, 21, 8],
+//  [13, 12, 11, 10, 9]]
+
+function matrix(n) {
+  //初始化n*n矩阵
+  const ret = []
+  for(let i=0; i< n; i++){
+    ret.push([])
+  }
+  // 初始化填充数字
+  let counter = 1
+  // 初始化行列下标值
+  let srow = 0
+  let erow = n - 1
+  let scol = 0
+  let ecol = n - 1
+  //根据行列值变化循环转圈填充
+  while (srow <= erow && scol <= erow) {
+    //向右→填充
+    for(let i=scol; i<=ecol; i++){
+      ret[srow][i] = counter++
+    }
+    //该行填充完向下移一行
+    srow++
+
+    //向下↓填充
+    for(let i=srow; i <= erow; i++){
+      ret[i][ecol] = counter++
+    }
+    //该列填充完向左移一列
+    ecol--
+
+    //向左←填充
+    for(let i=ecol; i >= scol; i--){
+      ret[erow][i] = counter++
+    }
+    //该行填充完向上移一行
+    erow--
+
+    //向上↑填充
+    for(let i=erow; i>=srow; i--){
+      ret[i][scol] = counter++
+    }
+    //该列填充完向右移一列
+    scol++
+  }
+  return ret
+}
 
 module.exports = matrix;
