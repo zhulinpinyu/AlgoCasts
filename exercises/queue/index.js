94ffebd46d5ccc5d0254766dafc2663ad2ffdd83
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-15 10:12:07 +0800

    实现队列Queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,37 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// 实现队列Queue Solution #1
+// class Queue {
+//     constructor() {
+//         this.data = []
+//     }
+
+//     add(e) {
+//         this.data.push(e)
+//     }
+
+//     remove() {
+//         return this.data.shift()
+//     }
+
+// }
+
+
+// 实现队列Queue Solution #2
+class Queue {
+    constructor() {
+        this.data = []
+    }
+
+    add(e) {
+        this.data.unshift(e)
+    }
+
+    remove() {
+        return this.data.pop()
+    }
+
+}
 
 module.exports = Queue;
