9456c695d556346c6f156aa11d35047f963a2bc0
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-18 11:51:34 +0800

    getFirst & getLast node of LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -28,6 +28,18 @@ class LinkedList {
 
         return count
     }
+
+    getFirst() {
+        return this.head
+    }
+
+    getLast() {
+        let node = this.head
+        while (node.next) {
+            node = node.next
+        }
+        return node
+    }
 }
 
 module.exports = { Node, LinkedList };
