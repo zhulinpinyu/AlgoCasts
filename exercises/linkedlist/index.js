9e5718609397298a90f52088ae15642d7730b4af
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-18 12:13:43 +0800

    update getLast of LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -35,7 +35,7 @@ class LinkedList {
 
     getLast() {
         let node = this.head
-        while (node.next) {
+        while (node && node.next) {
             node = node.next
         }
         return node
