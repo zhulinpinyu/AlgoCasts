c4a21a1fe8eb69bbe069557039dbe97af77cab44
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-19 13:59:34 +0800

    计算Tree中每一层的节点个数

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,38 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// function levelWidth(root) {
+//     let arr = [root, '*']
+//     let ret = [0]
+//     while (arr.length > 1) {
+//         let node = arr.shift()
+//         if (node === '*') {
+//             ret.push(0)
+//             arr.push('*')
+//         } else {
+//             arr.push(...node.children)
+//             ret[ret.length - 1]++
+//         }
+//     }
+//     return ret
+// }
+
+
+//重复
+const levelWidth = (root) => {
+    let arr = [root, '*']
+    let ret = [0]
+    while (arr.length > 1) {
+        let node = arr.shift()
+        if (node === '*') {
+            arr.push('*')
+            ret.push(0)
+        } else {
+            arr.push(...node.children)
+            ret[ret.length - 1]++
+        }
+    }
+    return ret
+}
 
 module.exports = levelWidth;
