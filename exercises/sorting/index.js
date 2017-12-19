cbe47a8cb842a9277502b4a1d2f25a939df1d3f8
zhulinpinyu <mulixiang199016@gmail.com>
2017-12-19 16:13:29 +0800

    selectionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -15,7 +15,18 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let min = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j
+            }
+        }
+        let tmp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = tmp
+    }
+    return arr
 }
 
 function mergeSort(arr) {
